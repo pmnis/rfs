6d12e2f97dec40b549e2e67237abd6a08cc24ada
Pierre Morel <pmorel@mnis.fr>
2019-06-24 16:52:01 +0200

    shell: assume qemu is in the PATH
    Signed-off-by: Pierre Morel <pmorel@mnis.fr>

--- start_initrd.sh
+++ start_initrd.sh
@@ -6,12 +6,14 @@ RFS=${1:-rfs00.gz}
 echo $RFS
 shift
 
-/usr/local/bin/qemu-system-x86_64 \
-     -machine pc,accel=kvm \
-     -enable-kvm -smp 2 -m 512 \
-     -netdev tap,id=hn0,queues=1 \
-     -device virtio-net,netdev=hn0,mq \
-     -kernel /boot/vmlinuz-${SUFFIXE} \
-     -initrd ${RFS} \
-     -append "console=ttyS0 " \
-     -nographic $*
+qemu-system-x86_64 \
+	-machine pc,accel=kvm \
+	-enable-kvm -smp 2 -m 1G \
+	-netdev tap,id=hn0,queues=1 \
+	-device virtio-net,netdev=hn0,mq \
+	-drive id=drv0,file=dsk0,format=raw,if=none \
+	-device virtio-blk,drive=drv0,id=dev0 \
+	-kernel /boot/vmlinuz-${SUFFIXE} \
+	-initrd ${RFS} \
+	-append "console=ttyS0" \
+	-nographic $*
