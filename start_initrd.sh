b424bfbb7c2dffd310c0c32e5d9fc8d32fc1ab2c
Pierre Morel <pmorel@mnis.fr>
2019-07-23 16:49:23 +0200

    start: x86: remove the hard path for qemu
    Signed-off-by: Pierre Morel <pmorel@mnis.fr>

--- start_initrd.sh
+++ start_initrd.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-SUFFIXE=$(cat config/version.cfg)
+SUFFIXE=$(cat config/version.cfg 2>/dev/null)
+[[ $SUFFIXE ]] || SUFFIXE=$(uname -r)
 RFS=${1:-rfs00.gz}
 
 echo $RFS
 shift
 
-/usr/local/bin/qemu-system-x86_64 \
+qemu-system-x86_64 \
      -machine pc,accel=kvm \
      -enable-kvm -smp 2 -m 512 \
      -kernel /boot/vmlinuz-${SUFFIXE} \
